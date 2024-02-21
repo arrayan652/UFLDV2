afa153eb4ee975068dac41edc1b58498f357b2e2
daffadha <92097485+arrayan652@users.noreply.github.com>
2024-02-21 16:04:26 +0800

    convert_tusimple.py

--- scripts/convert_tusimple.py
+++ scripts/convert_tusimple.py
@@ -90,7 +90,7 @@ def generate_segmentation_and_train_list(root, line_txt, names):
 
         label_path = names[i][:-3]+'png'
         label = np.zeros((720,1280),dtype=np.uint8)
-        all_points = np.zeros((4,56,2), dtype=np.float)
+        all_points = np.zeros((4,56,2), dtype=float)
         the_anno_row_anchor = np.array([160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260,
        270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370,
        380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480,
